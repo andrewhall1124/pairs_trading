ed2f10809648971672cba967078f2a755f627251
Andrew Hall <andrewmartinhall2@gmail.com>
2024-11-14 15:52:46 -0700

    Added dynamic root directory to notebooks

---
+++ research/notebooks/config.py
@@ -0,0 +1,12 @@
+import os
+
+def set_project_root(levels_up=2):
+    """
+    Sets the working directory to the project root.
+    
+    Parameters:
+    levels_up (int): Number of levels to go up in the directory structure to reach the root.
+    """
+    current_dir = os.getcwd()
+    project_root = os.path.abspath(os.path.join(current_dir, *[".."] * levels_up))
+    os.chdir(project_root)
