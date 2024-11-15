ff6d36236fbfbdd0f3fa07cba1d84a8a2f600c40
Andrew Hall <andrewmartinhall2@gmail.com>
2024-11-15 07:50:27 -0700

    Rearranged files

---
+++ research/strategies/pairs_trading/notebooks/config.py
@@ -0,0 +1,15 @@
+import os
+
+def set_project_root(levels_up=2, print_path: bool = False):
+    """
+    Sets the working directory to the project root.
+    
+    Parameters:
+    levels_up (int): Number of levels to go up in the directory structure to reach the root.
+    """
+    current_dir = os.getcwd()
+    project_root = os.path.abspath(os.path.join(current_dir, *[".."] * levels_up))
+    os.chdir(project_root)
+
+    if print_path:
+        print(project_root)
